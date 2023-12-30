85510a8766165cfba63ea9143a7f9ed04e23120d
sorcererak <153740731+sorcererak@users.noreply.github.com>
2023-12-30 23:33:28 +0530

    Assg3_Q1 main file

--- main.dart
+++ main.dart
@@ -1,319 +1,268 @@
-
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:toast/toast.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 100, 34, 255)),
-        ),
-        home: MyHomePage(),
+    return MaterialApp(
+      title: 'Material Design Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      // home: MaterialDesign(),
+      initialRoute: '/',
+      routes: {
+        '/':(context) => MaterialDesign(),
+        '/first': (context) => const firstRoute(),
+      },
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
-
-  var favorites = <WordPair>[];
-
-  void toggleFavorite() {
-    if (favorites.contains(current)) {
-      favorites.remove(current);
-    } else {
-      favorites.add(current);
-    }
-
-
-    notifyListeners();
-  }
-
-  void removeFavourite(WordPair Favourite){
-    favorites.remove(Favourite);
-    notifyListeners();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
+class MaterialDesign extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MaterialDesignState createState() => _MaterialDesignState();
+  
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MaterialDesignState extends State<MaterialDesign> {
 
-  var selectedIndex=0;
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController rollNoController = TextEditingController();
+  TextEditingController phoneNumberController = TextEditingController();
+
+
   @override
   Widget build(BuildContext context) {
+        ToastContext().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.person),
+        backgroundColor: Colors.blue,
+        title: Text('My App Demo'),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
 
-    Widget page;
-    switch (selectedIndex) {
-      case 0:
-        page = GeneratorPage();
-        break;
-      case 1:
-        page = FavoritesPage();
-        break;
-      case 2:
-        page = ProfilePage();
-        break;
-      default:
-        throw UnimplementedError('no widget for $selectedIndex');
-    }
-
-    bool isMenuExtended= false;
-
-    void toggleElevatedButton() {
-      setState(() {
-        isMenuExtended = !isMenuExtended;
-      });
-    }
+            SizedBox(height: 16.0),
 
 
-   return Scaffold(
-      body: Row(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(left: isMenuExtended ? 200.0 : 8.0, bottom: 15.0),
-            child: SafeArea(
-              child: NavigationRail(
-                extended: !isMenuExtended,
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.person),
-                    label: Text('Profile'),
-                  ),
-                ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
+              AppBar(
+                title: Text('Input User Details'),
+                backgroundColor: Colors.lightBlue[100],
+            ),
+
+
+            SizedBox(height: 16.0),
+
+
+            Card(
+              child: Container(
+                color: Color.fromARGB(255, 243, 33, 226),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min, // Set mainAxisSize to MainAxisSize.min
+                  crossAxisAlignment: CrossAxisAlignment.start, // Align children to the left
+                    children: [
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        labelText: 'Input Name:',
+                        labelStyle: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextField(
+                      controller: emailController,
+                      decoration: InputDecoration(
+                        labelText: 'Input Email:',
+                        labelStyle: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextField(
+                      controller: rollNoController,
+                      decoration: InputDecoration(
+                        labelText: 'Input Roll No:',
+                        labelStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0),
+                    TextField(
+                      controller: phoneNumberController,
+                      decoration: InputDecoration(
+                        labelText: 'Input Phone Number:',
+                        labelStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
           ),
-          Expanded(
-            child: Container(
-              color: Theme.of(context).colorScheme.primaryContainer,
-              child: page,
+
+          SizedBox(height: 16.0),
+          Center(
+            child: ElevatedButton(
+              child: Text('Show Details'),
+              onPressed: () {
+                Navigator.pushNamed(
+                    context,
+                    '/first',
+                    arguments: {
+                      'name': nameController.text,
+                      'email': emailController.text,
+                      'rollNo': rollNoController.text,
+                      'phoneNumber': phoneNumberController.text,
+                    },
+                  );
+              },
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            isMenuExtended = isMenuExtended;
-          });
-        },
-        child: Icon(Icons.menu),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startDocked,
-    );
-  }
-}
-
-
-class ProfilePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context){
-
-    return Column(
-      children: [
-        Center(
-          child: CircleAvatar(
-            radius: 200,
-            backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHE4vcD6O1-GAxEU2CDLEQlD140pQI94q5qA&usqp=CAU'),
           )
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(width:10),
-            Text(
-              'Akshay Reddy Kamatam',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
+            
+           
+
           ],
         ),
-        SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(width:10),
-            Text(
-              'areddy23@iitk.ac.in',
-              style: TextStyle(
-                fontSize: 8,
-                color: Colors.black
-              ),),
-          ],
-        ),
-        SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(width:20),
-            Icon(Icons.location_on),
-            Text('City: Kanpur')
-          ],
-        ),
-        SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(width:20),
-            Icon(Icons.assignment_ind),
-            Text('Roll No :230517'),
-          ],
-        )
-
-
-      ],
-    );
-  }
-}
-
-class FavoritesPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    if (appState.favorites.isEmpty) {
-      return Center(
-        child: Text('No favorites yet.'),
-      );
-    }
-
-    return ListView(
-      children: [
-        // Padding(
-        //   padding: const EdgeInsets.all(20),
-        //   child: Text('You have '
-        //       '${appState.favorites.length} favorites:'),
-        // ),
-        for (var pair in appState.favorites)
-          ListTile(
-            leading: Icon(Icons.favorite),
-            title:Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(pair.asCamelCase),
-                GestureDetector(
-                  onTap: (){
-                      context.read<MyAppState>().removeFavourite(pair);
-                  },
-                  child: Icon(Icons.delete)
-                )
-              ],
-            )
-          ),
-      ],
-    );
-  }
-}
-
-
-class GeneratorPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          BigCard(pair: pair),
-          SizedBox(height: 10),
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  appState.toggleFavorite();
-                },
-                icon: Icon(icon),
-                label: Text('Like'),
-              ),
-              SizedBox(width: 10),
-              ElevatedButton(
-                onPressed: () {
-                  appState.getNext();
-                },
-                child: Text('Next'),
-              ),
-            ],
-          ),
-        ],
       ),
     );
   }
 }
 
-
-
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
-
-  final WordPair pair;
-
-   @override
+class firstRoute extends StatelessWidget {
+  const firstRoute({Key? key}) : super(key: key);
+  @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);       // ← Add this.
-     // ↓ Add this.
-    final style = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-    );
 
+    final Map<String, String> userDetails =
+        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
 
-    return Card(
-      color: theme.colorScheme.primary,    // ← And also this.
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase,style: style),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Details Are'),
+        backgroundColor: Color.fromARGB(255, 76, 175, 87),
       ),
-    );
+      
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+
+            SizedBox(height: 16.0,),
+
+            Card(
+              child:Container(
+                color: Color.fromARGB(255, 243, 33, 226),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min, // Set mainAxisSize to MainAxisSize.min
+                  crossAxisAlignment: CrossAxisAlignment.start, // Align children to the left
+                    children: [
+                      Center(
+                        child: CircleAvatar(
+                          radius: 100,
+                          backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHE4vcD6O1-GAxEU2CDLEQlD140pQI94q5qA&usqp=CAU')
+                        ),
+                      ),
+                      Text(
+                        'Name: ${userDetails['name']}',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),),
+
+                      SizedBox(height: 16.0,),  
+
+                      Text(
+                        'Email: ${userDetails['email']}',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                        ),
+
+                      SizedBox(height: 16.0,),
+
+                      Text(
+                        'Roll No: ${userDetails['rollNo']}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),),
+
+                      SizedBox(height: 16.0,),
+
+                      Text(
+                        'Phone Number: ${userDetails['phoneNumber']}',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+
+                      SizedBox(height: 16.0,),
+
+                    ]
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 16.0,),
+                    Center(
+                        child: ElevatedButton(
+                                child: const Text('Go back'),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                   showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                      title: Text('Dialog Title'),
+                                      content: Text('User {${userDetails['name']}} Logged out'),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                          Navigator.pop(context);
+                                          },
+                                        child: Text('Close'),
+                                        )
+                                      ]
+                                      );
+                                    }
+                                   );
+                                },
+                              ),
+                      )
+                  ],
+                ),
+              ),
+            );
   }
 }
